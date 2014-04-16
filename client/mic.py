ac5db8050c48d5fe9126c41fc4d7032be99568e8
exadrid <exadrid@gmail.com>
2014-04-16 16:55:27 -0400

    Error

--- client/mic.py
+++ client/mic.py
@@ -65,7 +65,7 @@ class Mic:
             self.speechRec_persona.decode_raw(wavFile)
             result = self.speechRec_persona.get_hyp()
         elif GOOGLE:
-            os.system("ffmpeg -i active.wav -ar 16000 -acodec flac active.flac" % (RATE, audio_file_path))
+            os.system("ffmpeg -i active.wav -ar 16000 -acodec flac active.flac")
             print "###1###"
             flac = open("active.flac", 'rb')
             data = flac.read()
