c519aa287ed2e354b82699aca3960319487c0bf1
Deivuh <dyhl22@gmail.com>
2014-05-29 02:49:32 +0000

    Added /usr/local/bin PATH inclusion, fixes Phonetisaurus call issue

--- boot/boot.sh
+++ boot/boot.sh
@@ -1,6 +1,6 @@
 cd /home/pi/jasper/boot/
 LD_LIBRARY_PATH="/usr/local/lib"
 export LD_LIBRARY_PATH
-PATH=$PATH:/usr/local/lib/
+PATH=$PATH:/usr/local/lib/:/usr/local/bin
 export PATH
 python boot.py &
