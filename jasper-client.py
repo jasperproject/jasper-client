3e3577ff79a93b387b6e9dd7c4a02e08f180958a
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2014-09-01 22:53:58 +0200

    main program is a class now

--- jasper-client.py
+++ jasper-client.py
@@ -8,7 +8,6 @@ import traceback
 import shutil
 
 import yaml
-import client.vocabcompiler
 import client.speaker 
 from client.conversation import Conversation
 import client.jasperpath as jasperpath
@@ -26,18 +25,71 @@ if args.local:
 else:
     from client.mic import Mic
 
-def has_network_connection():
-    try:
-        # see if we can resolve the host name -- tells us if there is
-        # a DNS listening
-        host = socket.gethostbyname("www.google.com")
-        # connect to the host -- tells us if the host is actually
-        # reachable
-        s = socket.create_connection((host, 80), 2)
-    except:
-        return False
-    else:
-        return True
+class Jasper(object):
+    def __init__(self, compile=False):
+        self.create_config_dirs()
+
+        # Load config
+        self.config = yaml.safe_load(open(jasperpath.config("profile.yml"), "r"));
+
+        try:
+            client.stt.HMM_PATH = self.config['advanced_settings']['hmm']
+        except KeyError:
+            pass
+
+        try:
+            client.g2p.PHONETISAURUS_MODEL = self.config['advanced_settings']['fst']
+        except KeyError:
+            pass
+
+        try:
+            api_key = self.config['keys']['GOOGLE_SPEECH']
+        except KeyError:
+            api_key = None
+
+        try:
+            stt_engine_type = self.config['stt_engine']
+        except KeyError:
+            print "stt_engine not specified in profile, defaulting to PocketSphinx"
+            stt_engine_type = "sphinx"
+
+        # create Audio IO
+        speaker = client.speaker.newSpeaker()
+        passive_stt_engine = client.stt.PocketSphinxSTT(compile=compile)
+        active_stt_engine = client.stt.newSTTEngine(stt_engine_type, api_key=api_key)
+        self.audio = Mic(speaker, passive_stt_engine, active_stt_engine)
+
+    @property
+    def has_network_connection(self):
+        try:
+            # see if we can resolve the host name -- tells us if there is
+            # a DNS listening
+            host = socket.gethostbyname("www.google.com")
+            # connect to the host -- tells us if the host is actually
+            # reachable
+            s = socket.create_connection((host, 80), 2)
+        except:
+            return False
+        else:
+            return True
+
+    def create_config_dirs(self):
+        for directory in [jasperpath.CONFIG_PATH, jasperpath.config("sentences"), jasperpath.config("languagemodels"), jasperpath.config("dictionaries")]:
+            if not os.path.exists(directory):
+                try:
+                    os.mkdir(directory)
+                except OSError(errno, strerror):
+                    raise
+                else:
+                    pass
+
+    def start_conversation(self):
+        addendum = ""
+        if 'first_name' in self.config:
+            addendum = ", %s" % self.config["first_name"]
+        self.audio.say("How can I be of service%s?" % addendum)
+        conversation = Conversation("JASPER", self.audio, self.config)
+        conversation.handleForever()
 
 if __name__ == "__main__":
     print "==========================================================="
@@ -45,61 +97,15 @@ if __name__ == "__main__":
     print " Copyright 2013 Shubhro Saha & Charlie Marsh               "
     print "==========================================================="
 
-    speaker = client.speaker.newSpeaker()
-    speaker.say("Hello.... I am Jasper... Please wait one moment.")
-
-    for directory in [jasperpath.CONFIG_PATH, jasperpath.config("sentences"), jasperpath.config("languagemodels"), jasperpath.config("dictionaries")]:
-        if not os.path.exists(directory):
-            try:
-                os.mkdir(directory)
-            except OSError(errno, strerror):
-                print("Can't create configdir '%s': %s" % (directory, strerror))
-                speaker.say("Hello, I could not access the configuration directory or one of its subdirectories. Please check if you have the right permissions.")
-                sys.exit(1)
-
-    profile = yaml.safe_load(open(jasperpath.config("profile.yml"), "r"));
-
-    try:
-        client.stt.HMM_PATH = profile['advanced_settings']['hmm']
-    except KeyError:
-        pass
-
-    try:
-        client.g2p.PHONETISAURUS_MODEL = profile['advanced_settings']['fst']
-    except KeyError:
-        pass
+    app = Jasper(compile=args.compile)
 
     if args.checknetwork:
-        if has_network_connection():
+        if app.has_network_connection():
             print "CONNECTED TO INTERNET"
         else:
             print "COULD NOT CONNECT TO NETWORK"
             traceback.print_exc()
-            speaker.say("Hello, I could not connect to a network. Please read the documentation to configure your network.")
+            app.audio.say("Hello, I could not connect to a network. Please read the documentation to configure your network.")
             sys.exit(1)
 
-    if args.compile:
-        print "COMPILING DICTIONARY"
-        client.vocabcompiler.compile()
-
-    try:
-        api_key = profile['keys']['GOOGLE_SPEECH']
-    except KeyError:
-        api_key = None
-
-    try:
-        stt_engine_type = profile['stt_engine']
-    except KeyError:
-        print "stt_engine not specified in profile, defaulting to PocketSphinx"
-        stt_engine_type = "sphinx"
-
-    mic = Mic(speaker, client.stt.PocketSphinxSTT(), client.stt.newSTTEngine(stt_engine_type, api_key=api_key))
-
-    addendum = ""
-    if 'first_name' in profile:
-        addendum = ", %s" % profile["first_name"]
-    mic.say("How can I be of service%s?" % addendum)
-
-    conversation = Conversation("JASPER", mic, profile)
-
-    conversation.handleForever()
+    app.start_conversation()
