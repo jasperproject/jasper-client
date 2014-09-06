7d3901dcd25ec050dee8e0d31d5b86ca04f06c5e
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2014-09-06 10:38:55 +0200

    Use executable flag and shebang instead of explicit interpreter in shell scripts.

--- boot/boot.sh
+++ boot/boot.sh
@@ -14,4 +14,4 @@ LD_LIBRARY_PATH="/usr/local/lib"
 export LD_LIBRARY_PATH
 PATH=$PATH:/usr/local/lib/
 export PATH
-python2 boot.py &
+./boot.py &
--- client/start.sh
+++ client/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $JASPER_HOME/jasper/client
 rm -rf ../old_client
-python2 main.py &
+./main.py &
