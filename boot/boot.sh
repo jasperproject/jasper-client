332a6efa9e6edfc5322a179f5ead2c3a79d03274
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2014-09-06 10:32:43 +0200

    Add shebang to start.sh

--- boot/boot.sh
+++ boot/boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This file exists for backwards compatibility with older versions of jasper.
+# This file exists for backwards compatibility with older versions of Jasper.
 # It might be removed in future versions.
 "${0%/*}/boot.py"
