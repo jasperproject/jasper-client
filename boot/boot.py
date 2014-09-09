4aa42aad721b8e55f8a99db5acdb56d653eea5f5
Charles Marsh <charlie@khanacademy.org>
2014-09-09 12:09:35 -0400

    Better error message granularity in boot.py

--- boot/boot.py
+++ boot/boot.py
@@ -13,21 +13,43 @@ sys.path.append(lib_path)
 import speaker as speak
 speaker = speak.newSpeaker()
 
+
+def fail(message):
+    traceback.print_exc()
+    speaker.say(message)
+
+
 def configure():
     try:
         urllib2.urlopen("http://www.google.com").getcode()
 
         print "CONNECTED TO INTERNET"
         print "COMPILING DICTIONARY"
-        vocabcompiler.compile("../client/sentences.txt", "../client/dictionary.dic", "../client/languagemodel.lm")
+        vocabcompiler.compile(
+            "../client/sentences.txt", "../client/dictionary.dic", "../client/languagemodel.lm")
 
         print "STARTING CLIENT PROGRAM"
         os.system("$JASPER_HOME/jasper/client/start.sh &")
 
-    except:
+    except urllib2.URLError:
         print "COULD NOT CONNECT TO NETWORK"
-        traceback.print_exc()
-        speaker.say("Hello, I could not connect to a network. Please read the documentation to configure your Raspberry Pi.")
+        fail(
+            "I could not connect to a network. Please read the documentation to configure your Raspberry Pi.")
+
+    except OSError:
+        print "BOOT FAILURE: OSERROR"
+        fail(
+            "There was a problem starting Jasper. You may be missing the language model and associated files. Please read the documentation to configure your Raspberry Pi.")
+
+    except IOError:
+        print "BOOT FAILURE: IOERROR"
+        fail(
+            "There was a problem starting Jasper. You may have set permissions incorrectly on some part of the filesystem. Please read the documentation to configure your Raspberry Pi.")
+
+    except:
+        print "BOOT FAILURE"
+        fail(
+            "There was a problem starting Jasper. Please read the documentation to configure your Raspberry Pi.")
 
 if __name__ == "__main__":
     print "==========STARTING JASPER CLIENT=========="
