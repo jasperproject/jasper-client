ff34c9d7a4a1b6ffeae08efe7c4846c33e4c0673
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2014-09-10 17:30:34 +0200

    Move vocabcompiler into client folder

--- boot/boot.py
+++ boot/boot.py
@@ -28,14 +28,13 @@ else:
     path = "/usr/local/lib/"
 os.environ["PATH"] = path
 
-import urllib2
-import vocabcompiler
 import traceback
 
 lib_path = os.path.abspath('../client')
 sys.path.append(lib_path)
 
 from diagnose import Diagnostics
+import vocabcompiler
 import speaker as speak
 speaker = speak.newSpeaker()
 
