024b3b117a16eb802fb0e3fdfdcd7b564fbba5a5
Jan Holthuis <holthuis.jan@googlemail.com>
2014-10-01 20:05:32 +0200

    Fix bug in main.py
    This fixes #183

--- client/main.py
+++ client/main.py
@@ -6,6 +6,5 @@ import os
 import sys
 import runpy
 script_path = os.path.join(os.path.dirname(__file__), os.pardir, "jasper.py")
-sys.path.remove(os.path.dirname(__file__))
 sys.path.insert(0, os.path.dirname(script_path))
 runpy.run_path(script_path, run_name="__main__")
