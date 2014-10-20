c12b2ebebd63c28cd67e748e79e7e876fced6512
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2014-10-20 17:16:54 +0200

    Added MaryTTS engine

--- client/tts.py
+++ client/tts.py
@@ -15,6 +15,9 @@ import subprocess
 import pipes
 import logging
 import wave
+import urllib
+import urlparse
+import requests
 from abc import ABCMeta, abstractmethod
 
 import argparse
@@ -325,6 +328,74 @@ class GoogleTTS(AbstractMp3TTSEngine):
             tmpfile = f.name
         tts.save(tmpfile)
         self.play_mp3(tmpfile)
+        os.remove(tmpfile)
+
+
+class MaryTTS(AbstractTTSEngine):
+    """
+    Uses the MARY Text-to-Speech System (MaryTTS)
+    MaryTTS is an open-source, multilingual Text-to-Speech Synthesis platform
+    written in Java.
+    Please specify your own server instead of using the demonstration server
+    (http://mary.dfki.de:59125/) to save bandwidth and to protect your privacy.
+    """
+
+    SLUG = "mary-tts"
+
+    def __init__(self, server="mary.dfki.de", port="59125", language="en_GB",
+                 voice="dfki-spike"):
+        super(self.__class__, self).__init__()
+        self.server = server
+        self.port = port
+        self.netloc = '{server}:{port}'.format(server=self.server,
+                                               port=self.port)
+        self.language = language
+        self.voice = voice
+        self.session = requests.Session()
+
+    @property
+    def languages(self):
+        r = self.session.get(self._makeurl('/locales'))
+        r.raise_for_status()
+        return r.text.splitlines()
+
+    @property
+    def voices(self):
+        r = self.session.get(self._makeurl('/voices'))
+        r.raise_for_status()
+        return [line.split()[0] for line in r.text.splitlines()]
+
+    @classmethod
+    def is_available(cls):
+        return (super(cls, cls).is_available() and
+                diagnose.check_network_connection())
+
+    def _makeurl(self, path, query={}):
+        query_s = urllib.urlencode(query)
+        urlparts = ('http', self.netloc, path, query_s, '')
+        return urlparse.urlunsplit(urlparts)
+
+    def say(self, phrase):
+        self._logger.debug("Saying '%s' with '%s'", phrase, self.SLUG)
+        if self.language not in self.languages:
+            raise ValueError("Language '%s' not supported by '%s'"
+                             % (self.language, self.SLUG))
+
+        if self.voice not in self.voices:
+            raise ValueError("Voice '%s' not supported by '%s'"
+                             % (self.voice, self.SLUG))
+        query = {'OUTPUT_TYPE': 'AUDIO',
+                 'AUDIO': 'WAVE_FILE',
+                 'INPUT_TYPE': 'TEXT',
+                 'INPUT_TEXT': phrase,
+                 'LOCALE': self.language,
+                 'VOICE': self.voice}
+
+        r = self.session.get(self._makeurl('/process', query=query))
+        with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as f:
+            f.write(r.content)
+            tmpfile = f.name
+        self.play(tmpfile)
         os.remove(tmpfile)
 
 
