b969d376add9df2f325a736272089bf20e972942
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2015-01-12 13:35:20 +0100

    Julius STT error logging improved

--- client/stt.py
+++ client/stt.py
@@ -195,6 +195,31 @@ class JuliusSTT(AbstractSTTEngine):
         self._tiedlist = tiedlist
         self._pattern = re.compile(r'sentence(\d+): <s> (.+) </s>')
 
+        # Inital test run: we run this command once to log errors/warnings
+        cmd = ['julius',
+               '-input', 'stdin',
+               '-dfa', self._vocabulary.dfa_file,
+               '-v', self._vocabulary.dict_file,
+               '-h', self._hmmdefs,
+               '-hlist', self._tiedlist,
+               '-forcedict']
+        cmd = [str(x) for x in cmd]
+        self._logger.debug('Executing: %r', cmd)
+        with tempfile.SpooledTemporaryFile() as out_f:
+            with tempfile.SpooledTemporaryFile() as f:
+                with tempfile.SpooledTemporaryFile() as err_f:
+                    subprocess.call(cmd, stdin=f, stdout=out_f, stderr=err_f)
+            out_f.seek(0)
+            for line in out_f.read().splitlines():
+                line = line.strip()
+                if len(line) > 7 and line[:7].upper() == 'ERROR: ':
+                    if not line[7:].startswith('adin_'):
+                        self._logger.error(line[7:])
+                elif len(line) > 9 and line[:9].upper() == 'WARNING: ':
+                    self._logger.warning(line[9:])
+                elif len(line) > 6 and line[:6].upper() == 'STAT: ':
+                    self._logger.debug(line[6:])
+
     @classmethod
     def get_config(cls):
         # FIXME: Replace this as soon as we have a config module
@@ -225,21 +250,15 @@ class JuliusSTT(AbstractSTTEngine):
                '-forcedict']
         cmd = [str(x) for x in cmd]
         self._logger.debug('Executing: %r', cmd)
-        transcribed = []
         with tempfile.SpooledTemporaryFile() as out_f:
             with tempfile.SpooledTemporaryFile() as err_f:
                 subprocess.call(cmd, stdin=fp, stdout=out_f, stderr=err_f)
             out_f.seek(0)
-            output = out_f.read()
-            for line in output.splitlines():
-                line = line.strip()
-                if line:
-                    if line.startswith('sentence'):
-                        print(line)
-                        matchobj = self._pattern.search(line)
-                        if matchobj and matchobj.groups(1):
-                            transcribed.append(matchobj.group(2))
-                    self._logger.debug(line)
+            results = [(int(i), text) for i, text in
+                       self._pattern.findall(out_f.read())]
+        transcribed = [text for i, text in
+                       sorted(results, key=lambda x: x[0])
+                       if text]
         if not transcribed:
             transcribed.append('')
         self._logger.info('Transcribed: %r', transcribed)
