18f9e47a16f2d7c046c4a6b864c3532dd6dcb645
Rahul Arora <rahulxxarora@gmail.com>
2015-07-17 23:55:28 +0530

    Added WordMeaning module

---
+++ client/modules/WordMeanings.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8-*-
+from mewe import dic
+
+WORDS = ["FIND", "MEANING", "OF"]
+
+
+def getresults(text, mic):
+    query = ' '.join(s for s in text.split() if s.upper() not in WORDS)
+    results = dic.get(query)
+    print('Fetched results are.')
+    for i in results:
+        print(i)
+
+
+def handle(text, mic, profile):
+    """
+        Responds to user-input, by telling word meanings".
+        Arguments:
+        text -- user-input, ex - "Find meaning of 'elephant'"
+        mic -- used to interact with the user (for both input and output)
+        profile -- contains information related to the user
+    """
+    getresults(text, mic)
+
+
+def isValid(text):
+    """
+      Returns True if the input is related to flipkart.
+
+      Arguments:
+      text -- user-input, typically transcribed speech
+    """
+
+    return bool(re.search('Meaning', text, re.IGNORECASE))
