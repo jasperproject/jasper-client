627c894aaf4a72732d3d62fc8bc1bb4b7a29927e
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-07-22 08:21:25 -0500

    remove camel case from detect_years

--- client/alteration.py
+++ client/alteration.py
@@ -2,7 +2,7 @@
 import re
 
 
-def detectYears(input):
+def detect_years(input):
     YEAR_REGEX = re.compile(r'(\b)(\d\d)([1-9]\d)(\b)')
     return YEAR_REGEX.sub('\g<1>\g<2> \g<3>\g<4>', input)
 
