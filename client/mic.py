f6ee7b0ba522cdeb047625b88dcc72d6554ed736
Jitendra <olxboss@gmail.com>
2015-10-15 13:00:59 +0530

    Corrected indentation
    Corrected indentation

--- client/mic.py
+++ client/mic.py
@@ -260,7 +260,6 @@ class Mic:
         # alter phrase before speaking
         phrase = alteration.clean(phrase)
         self.speaker.say(phrase)
-    
+
     def play(self, soundFile):
-        self.speaker.play(jasperpath.data('audio', soundFile))    
-  
+        self.speaker.play(jasperpath.data('audio', soundFile))
