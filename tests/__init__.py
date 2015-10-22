417f6fb6501b713f72aebb44d580a093e6fb00b6
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2015-10-22 23:35:59 +0200

    Add discovery code for plugin testcases

---
+++ tests/__init__.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+
+PATTERN = "test_*.py"
+
+
+def load_tests(loader, tests, pattern):
+    if pattern is None:
+        # if loaded as a module just return the normal tests
+        return tests
+
+    # Add core tests (from this package)
+    core_tests_dir = os.path.dirname(os.path.abspath(__file__))
+    tests.addTests(loader.discover(core_tests_dir, PATTERN))
+
+    # Add plugin tests
+    plugin_dir = os.path.abspath(os.path.join(
+        core_tests_dir, os.path.pardir, "plugins"))
+    for name in os.listdir(plugin_dir):
+        path = os.path.join(plugin_dir, name)
+        if os.path.isdir(path):
+            plugin_loader = unittest.TestLoader()
+            plugin_tests = plugin_loader.discover(path, "test_*.py", path)
+            tests.addTests(plugin_tests)
+
+    return tests
