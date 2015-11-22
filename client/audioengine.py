17fc4cb71c9be5eec44ccdffc8295d6ce2581aff
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2015-11-22 20:32:24 +0100

    audioengine: Add error handling for IOErrors that occur during recording

--- client/audioengine.py
+++ client/audioengine.py
@@ -207,8 +207,22 @@ class PyAudioDevice(object):
     def record(self, chunksize, *args):
         with self._open_stream(*args, output=False) as stream:
             while True:
-                frame = stream.read(chunksize)
-                yield frame
+                try:
+                    frame = stream.read(chunksize)
+                except IOError as e:
+                    if type(e.errno) is not int:
+                        # Simple hack to work around the fact that the
+                        # errno/strerror arguments were swapped in older
+                        # PyAudio versions. This was fixed in upstream
+                        # commit 1783aaf9bcc6f8bffc478cb5120ccb6f5091b3fb.
+                        strerror, errno = e.errno, e.strerror
+                    else:
+                        strerror, errno = e.strerror, e.errno
+                    self._logger.warning("IO error while reading from device" +
+                                         " '%s': '%s' (Errno: %d)", self.slug,
+                                         strerror, errno)
+                else:
+                    yield frame
 
     def play_fp(self, fp):
         w = wave.open(fp, 'rb')
