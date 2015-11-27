ded392cfba03918cbec0568bdba3d63a486fbfb9
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2015-11-27 18:38:27 +0100

    audioengine: Add an abstract AudioEngine/AudioDevice base class

--- client/audioengine.py
+++ client/audioengine.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import abc
 import logging
 import contextlib
 import re
@@ -28,7 +29,119 @@ class DeviceNotFound(DeviceException):
     pass
 
 
-class PyAudioEngine(object):
+class AudioEngine(object):
+    @abc.abstractmethod
+    def get_devices(self, device_type=DEVICE_TYPE_ALL):
+        pass
+
+    @abc.abstractmethod
+    def get_default_device(self, output=True):
+        pass
+
+    @abc.abstractmethod
+    def get_device_by_slug(self, slug):
+        pass
+
+
+class AudioDevice(object):
+    def __init__(self, name):
+        self._name = name
+        self._slug = slugify.slugify(name)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def slug(self):
+        return self._slug
+
+    @abc.abstractproperty
+    def types(self):
+        pass
+
+    @abc.abstractmethod
+    def supports_format(self, bits, channels, rate, output=True):
+        pass
+
+    @abc.abstractmethod
+    @contextlib.contextmanager
+    def open_stream(self, bits, channels, rate, chunksize=1024, output=True):
+        pass
+
+    @abc.abstractmethod
+    def record(self, chunksize, *args):
+        with self.open_stream(*args, chunksize=chunksize,
+                              output=False) as stream:
+            while True:
+                try:
+                    frame = stream.read(chunksize)
+                except IOError as e:
+                    if type(e.errno) is not int:
+                        # Simple hack to work around the fact that the
+                        # errno/strerror arguments were swapped in older
+                        # PyAudio versions. This was fixed in upstream
+                        # commit 1783aaf9bcc6f8bffc478cb5120ccb6f5091b3fb.
+                        strerror, errno = e.errno, e.strerror
+                    else:
+                        strerror, errno = e.strerror, e.errno
+                    self._logger.warning("IO error while reading from device" +
+                                         " '%s': '%s' (Errno: %d)", self.slug,
+                                         strerror, errno)
+                else:
+                    yield frame
+
+    def play_fp(self, fp):
+        w = wave.open(fp, 'rb')
+        channels = w.getnchannels()
+        bits = w.getsampwidth()*8
+        rate = w.getframerate()
+        chunksize = 1024
+        with self.open_stream(bits, channels, rate,
+                              chunksize=chunksize) as stream:
+            data = w.readframes(chunksize)
+            while data:
+                stream.write(data)
+                data = w.readframes(chunksize)
+        w.close()
+
+    def play_file(self, filename):
+        with open(filename, 'rb') as f:
+            self.play_fp(f)
+
+    def print_device_info(self, verbose=False):
+        print('[Audio device \'%s\']' % self.slug)
+        print('  Name: %s' % self.name)
+        print('  Input device: %s' % ('Yes'
+                                      if DEVICE_TYPE_INPUT in self.types
+                                      else 'No'))
+        print('  Output device: %s' % ('Yes'
+                                       if DEVICE_TYPE_OUTPUT in self.types
+                                       else 'No'))
+        if verbose:
+            for io_type in self.types:
+                direction = 'output' if DEVICE_TYPE_OUTPUT else 'input'
+                print('  Supported %s formats:' % direction)
+                formats = []
+                for bits in (8, 16, 24, 32):
+                    for channels in (1, 2):
+                        for rate in STANDARD_SAMPLE_RATES:
+                            args = (bits, channels, rate)
+                            if self.supports_format(
+                                    *args, output=(direction == 'output')):
+                                formats.append(args)
+                if len(formats) == 0:
+                    print('    None')
+                else:
+                    n = 4
+                    for chunk in (formats[i:i+n]
+                                  for i in range(0, len(formats), n)):
+                        print('    %s' % ', '.join(
+                            "(%d Bit %d CH @ %d Hz)" % fmt
+                            for fmt in chunk))
+
+
+class PyAudioEngine(AudioEngine):
     _PYAUDIO_BIT_MAPPING = {8:  pyaudio.paInt8,
                             16: pyaudio.paInt16,
                             24: pyaudio.paInt24,
@@ -40,6 +153,7 @@ class PyAudioEngine(object):
             return cls._PYAUDIO_BIT_MAPPING[bits]
 
     def __init__(self):
+        super(PyAudioEngine, self).__init__()
         self._logger = logging.getLogger(__name__)
         self._logger.info("Initializing PyAudio. ALSA/Jack error messages " +
                           "that pop up during this process are normal and " +
@@ -89,29 +203,25 @@ class PyAudioEngine(object):
         raise DeviceNotFound("Audio device with slug '%s' not found" % slug)
 
 
-class PyAudioDevice(object):
+class PyAudioDevice(AudioDevice):
     RE_PRESLUG = re.compile(r'\(hw:\d,\d\)')
 
     def __init__(self, engine, info):
+        super(PyAudioDevice, self).__init__(info['name'])
         self._logger = logging.getLogger(__name__)
         self._engine = engine
         self._index = info['index']
-        self._name = info['name']
         self._max_output_channels = info['maxOutputChannels']
         self._max_input_channels = info['maxInputChannels']
         # slugify the name
-        preslug_name = self.RE_PRESLUG.sub('', info['name'])
+        preslug_name = self.RE_PRESLUG.sub('', self.name)
         if preslug_name.endswith(': - '):
-            preslug_name = info['name']
-        self._slug = slugify.slugify(preslug_name)
-
-    @property
-    def name(self):
-        return self._name
+            preslug_name = self.name
+        self._pyaudio_slug = slugify.slugify(preslug_name)
 
     @property
     def slug(self):
-        return self._slug
+        return self._pyaudio_slug
 
     @property
     def index(self):
@@ -153,7 +263,7 @@ class PyAudioDevice(object):
             return supported
 
     @contextlib.contextmanager
-    def _open_stream(self, bits, channels, rate, chunksize=1024, output=True):
+    def open_stream(self, bits, channels, rate, chunksize=1024, output=True):
         # Check if format is supported
         is_supported_fmt = self.supports_format(bits, channels, rate,
                                                 output=output)
@@ -180,6 +290,7 @@ class PyAudioDevice(object):
             'frames_per_buffer': chunksize if output else chunksize*8  # Hacky
         }
         stream = self._engine._pyaudio.open(**stream_kwargs)
+
         self._logger.debug("%s stream opened on device '%s' (%d Hz, %d " +
                            "channel, %d bit)", "output" if output else "input",
                            self.slug, rate, channels, bits)
@@ -191,8 +302,8 @@ class PyAudioDevice(object):
                                "output" if output else "input", self.slug)
 
     def record(self, chunksize, *args):
-        with self._open_stream(*args, chunksize=chunksize,
-                               output=False) as stream:
+        with self.open_stream(*args, chunksize=chunksize,
+                              output=False) as stream:
             while True:
                 try:
                     frame = stream.read(chunksize)
@@ -210,53 +321,3 @@ class PyAudioDevice(object):
                                          strerror, errno)
                 else:
                     yield frame
-
-    def play_fp(self, fp):
-        w = wave.open(fp, 'rb')
-        channels = w.getnchannels()
-        bits = w.getsampwidth()*8
-        rate = w.getframerate()
-        chunksize = 1024
-        with self._open_stream(bits, channels, rate,
-                               chunksize=chunksize) as stream:
-            data = w.readframes(chunksize)
-            while data:
-                stream.write(data)
-                data = w.readframes(chunksize)
-        w.close()
-
-    def play_file(self, filename):
-        with open(filename, 'rb') as f:
-            self.play_fp(f)
-
-    def print_device_info(self, verbose=False):
-        print('[Audio device \'%s\']' % self.slug)
-        print('  ID: %d' % self.index)
-        print('  Name: %s' % self.name)
-        print('  Input device: %s' % ('Yes'
-                                      if DEVICE_TYPE_INPUT in self.types
-                                      else 'No'))
-        print('  Output device: %s' % ('Yes'
-                                       if DEVICE_TYPE_OUTPUT in self.types
-                                       else 'No'))
-        if verbose:
-            for io_type in self.types:
-                direction = 'output' if DEVICE_TYPE_OUTPUT else 'input'
-                print('  Supported %s formats:' % direction)
-                formats = []
-                for bits in (8, 16, 24, 32):
-                    for channels in (1, 2):
-                        for rate in STANDARD_SAMPLE_RATES:
-                            args = (bits, channels, rate)
-                            if self.supports_format(
-                                    *args, output=(direction == 'output')):
-                                formats.append(args)
-                if len(formats) == 0:
-                    print('    None')
-                else:
-                    n = 4
-                    for chunk in (formats[i:i+n]
-                                  for i in range(0, len(formats), n)):
-                        print('    %s' % ', '.join(
-                            "(%d Bit %d CH @ %d Hz)" % fmt
-                            for fmt in chunk))
