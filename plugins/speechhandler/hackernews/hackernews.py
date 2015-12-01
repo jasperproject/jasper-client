618ed0b72cbad7bfae76e185f1dcafe24c0183f3
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2015-12-01 17:31:19 +0100

    HackerNewsPlugin: Reimplement Hacker News plugin using the official Hacker News API

--- plugins/speechhandler/hackernews/hackernews.py
+++ plugins/speechhandler/hackernews/hackernews.py
@@ -1,142 +1,109 @@
 # -*- coding: utf-8 -*-
-import urllib2
-import re
-import random
-import bs4
+import collections
+import requests
 from client import app_utils
 from client import plugin
-from semantic.numbers import NumberService
 
-URL = 'http://news.ycombinator.com'
+HN_TOPSTORIES_URL = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+HN_ITEM_URL = 'https://hacker-news.firebaseio.com/v0/item/%d.json'
 
-
-class HNStory:
-
-    def __init__(self, title, URL):
-        self.title = title
-        self.URL = URL
+Article = collections.namedtuple('Article', ['title', 'link'])
 
 
-def get_top_stories(maxResults=None):
-    """
-        Returns the top headlines from Hacker News.
-
-        Arguments:
-        maxResults -- if provided, returns a random sample of size maxResults
-    """
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = urllib2.Request(URL, headers=hdr)
-    page = urllib2.urlopen(req).read()
-    soup = bs4.BeautifulSoup(page, 'html.parser')
-    matches = soup.findAll('td', class_="title")
-    matches = [m.a for m in matches if m.a and m.text != u'More']
-    matches = [HNStory(m.text, m['href']) for m in matches]
-
-    if maxResults:
-        num_stories = min(maxResults, len(matches))
-        return random.sample(matches, num_stories)
-
-    return matches
+def get_top_articles(num_headlines=5):
+    r = requests.get(HN_TOPSTORIES_URL)
+    item_ids = r.json()
+    articles = []
+    for i, item_id in enumerate(item_ids, start=1):
+        r = requests.get(HN_ITEM_URL % item_id)
+        item = r.json()
+        articles.append(Article(title=item['title'], link=item['url']))
+        if i >= num_headlines:
+            break
+    return articles
 
 
 class HackerNewsPlugin(plugin.SpeechHandlerPlugin):
+    def __init__(self, *args, **kwargs):
+        super(HackerNewsPlugin, self).__init__(*args, **kwargs)
+
+        try:
+            self._num_headlines = self.profile['hacker-news']['num-headlines']
+        except KeyError:
+            self._num_headlines = 3
+
     def get_priority(self):
         return 4
 
     def get_phrases(self):
-        return ["HACKER", "NEWS", "YES", "NO", "FIRST", "SECOND", "THIRD"]
+        return [
+            self.gettext("HACKER"),
+            self.gettext("NEWS"),
+            self.gettext("YES"),
+            self.gettext("NO")]
 
     def handle(self, text, mic):
         """
-        Responds to user-input, typically speech text, with a sample of
-        Hacker News's top headlines, sending them to the user over email
+        Responds to user-input, typically speech text, with a summary of
+        the day's top news headlines, sending them to the user over email
         if desired.
 
         Arguments:
         text -- user-input, typically transcribed speech
         mic -- used to interact with the user (for both input and output)
         """
-        mic.say("Pulling up some stories.")
-        stories = get_top_stories(maxResults=3)
-        all_titles = '... '.join(str(idx + 1) + ") " +
-                                 story.title for idx, story
-                                 in enumerate(stories))
+        mic.say(self.gettext("Getting the top %d stories from Hacker News...")
+                % self._num_headlines)
 
-        def handle_response(text):
+        articles = get_top_articles(num_headlines=self._num_headlines)
+        if len(articles) == 0:
+            mic.say(self.gettext(
+                "Sorry, I'm unable to get the top stories from " +
+                "Hacker News right now."))
+            return
 
-            def extract_ordinals(text):
-                output = []
-                service = NumberService()
-                for w in text.split():
-                    if w in service.__ordinals__:
-                        output.append(service.__ordinals__[w])
-                return [service.parse(w) for w in output]
+        text = self.gettext('These are the current top stories...')
+        text += ' '
+        text += '... '.join(
+            '%d) %s' % (i, a.title)
+            for i, a in enumerate(articles, start=1))
+        mic.say(text)
 
-            chosen_articles = extract_ordinals(text)
-            send_all = not chosen_articles and app_utils.is_positive(text)
+        if 'gmail_address' not in self.profile:
+            return
 
-            if send_all or chosen_articles:
-                mic.say("Sure, just give me a moment")
+        mic.say(self.gettext('Would you like me to send you these articles?'))
 
-                if self.profile['prefers_email']:
-                    body = "<ul>"
-
-                def formatArticle(article):
-                    tiny_url = app_utils.generate_tiny_URL(article.URL)
-
-                    if self.profile['prefers_email']:
-                        return "<li><a href=\'%s\'>%s</a></li>" % (
-                            tiny_url, article.title)
-                    else:
-                        return article.title + " -- " + tiny_url
-
-                for idx, article in enumerate(stories):
-                    if send_all or (idx + 1) in chosen_articles:
-                        article_link = formatArticle(article)
-
-                        if self.profile['prefers_email']:
-                            body += article_link
-                        else:
-                            if not app_utils.email_user(self.profile,
-                                                        SUBJECT="",
-                                                        BODY=article_link):
-                                mic.say("I'm having trouble sending you " +
-                                        "these articles. Please make sure " +
-                                        "that your phone number and carrier " +
-                                        "are correct on the dashboard.")
-                                return
-
-                # if prefers email, we send once, at the end
-                if self.profile['prefers_email']:
-                    body += "</ul>"
-                    if not app_utils.email_user(self.profile,
-                                                SUBJECT="From the Front " +
-                                                        "Page of Hacker News",
-                                                BODY=body):
-                        mic.say("I'm having trouble sending you these " +
-                                "articles. Please make sure that your " +
-                                "phone number and carrier are correct " +
-                                "on the dashboard.")
-                        return
-
-                mic.say("All done.")
-
+        answers = mic.active_listen()
+        if any(self.gettext('YES') in answer for answer in answers):
+            mic.say(self.gettext("Sure, just give me a moment."))
+            email_text = self.make_email_text(articles)
+            email_sent = app_utils.email_user(
+                self.profile,
+                SUBJECT=self.gettext("Top Stories from Hacker News"),
+                BODY=email_text)
+            if email_sent:
+                mic.say(self.gettext(
+                    "Okay, I've sent you an email."))
             else:
-                mic.say("OK I will not send any articles")
-
-        if not self.profile['prefers_email'] and self.profile['phone_number']:
-            mic.say("Here are some front-page articles. " +
-                    all_titles + ". Would you like me to send you these? " +
-                    "If so, which?")
-            handle_response(mic.active_listen()[0])
+                mic.say(self.gettext(
+                    "Sorry, I'm having trouble sending you these articles."))
         else:
-            mic.say("Here are some front-page articles. " + all_titles)
+            mic.say(self.gettext("Okay, I will not send any articles."))
+
+    def make_email_text(self, articles):
+        text = self.gettext(
+            'These are the Hacker News articles you requested:')
+        text += '\n\n'
+        for article in articles:
+            text += '- %s\n  %s\n' % (article.title, article.link)
+        return text
 
     def is_valid(self, text):
         """
-        Returns True if the input is related to Hacker News.
+        Returns True if the input is related to the news.
 
         Arguments:
         text -- user-input, typically transcribed speech
         """
-        return bool(re.search(r'\b(hack(er)?|HN)\b', text, re.IGNORECASE))
+        return (self.gettext('HACKER').upper() in text.upper())
