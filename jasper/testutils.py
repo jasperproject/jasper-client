8f5548800e2ff4017bb7704b37f56ea122b2c833
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2016-03-26 20:23:08 +0100

    testutils: Add TestConfiguration helper class

--- jasper/testutils.py
+++ jasper/testutils.py
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
+import os.path
 from . import paths
 from . import pluginstore
-from .config import Configuration
+from . import config
 
-TEST_CONFIG = Configuration([])
-TEST_CONFIG.read_defaults([paths.data('defaults.cfg')])
+
+class TestConfiguration(config.Configuration):
+    def __init__(self):
+        config.Configuration.__init__(self)
+        self.read_defaults([paths.data('defaults.cfg')])
+
+    def set(self, section, option, value):
+        if value is None:
+            if self._cp.has_section(section) and \
+                    self._cp.has_option(section, option):
+                self._cp.remove_option(section, option)
+        else:
+            if not self._cp.has_section(section):
+                self._cp.add_section(section)
+            self._cp.set(section, option, value)
+
+    def set_from_dict(self, data):
+        for section in data.keys():
+            for option, value in data[section].items():
+                self.set(section, option, value)
 
 
 class TestMic(object):
@@ -26,12 +45,16 @@ class TestMic(object):
         self.outputs.append(phrase)
 
 
-def get_plugin_instance(path, *extra_args):
+def get_plugin_instance(path, extra_args=[], extra_config={}):
     plugins = pluginstore.PluginStore([])
     plugin_info = plugins.parse_plugin(path)
-    TEST_CONFIG.read_defaults(
+
+    config = TestConfiguration()
+    config.read_defaults(
         plugin_info.default_config_file, map_sections=[
             ('Defaults', plugin_info.config_section)])
+    config.read(os.path.join(path, 'test.cfg'))
+    config.set_from_dict(extra_config)
 
-    args = tuple(extra_args) + (plugin_info, TEST_CONFIG)
+    args = tuple(extra_args) + (plugin_info, config)
     return plugin_info.plugin_class(*args)
